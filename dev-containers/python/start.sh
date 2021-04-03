cda5c8286283ddb9e85e366ff8ce9de8ed90ea90
Gareth Francis <gfrancis.dev@gmail.com>
2021-04-03 13:09:24 +0100

    Add dev env for python

---
+++ dev-containers/python/start.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+APP=python
+
+if [ $# -ne 1 ]; then
+  echo "USAGE: ./start.sh work-directory"
+  exit 1
+fi
+
+WORKDIR=$1
+RUNFLAGS="-v ${WORKDIR}:${WORKDIR}"
+
+thisdir=$(dirname "${BASH_SOURCE[0]}")
+
+cd ${thisdir}
+docker build --build-arg USER=${USER} -t ${APP}-env .
+docker run \
+    -it \
+    --rm \
+    --name ${APP} \
+    --net=host \
+    -e DISPLAY \
+    ${RUNFLAGS} \
+    -v ${HOME}/.Xauthority:/home/${USER}/.Xauthority \
+    ${APP}-env
+
